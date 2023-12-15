ee42f0722c6a5216f0cd6aa477a71129d2a8dcfd
Sean McNamara <smcnam@gmail.com>
2023-12-15 09:04:26 -0500

    Add MacOS image metadata and port build.gradle to Kotlin

---
+++ build.gradle.kts
@@ -0,0 +1,135 @@
+import org.gradle.internal.impldep.org.junit.experimental.categories.Categories.CategoryFilter.include
+import java.io.BufferedReader
+import java.io.InputStream
+import java.io.PrintStream
+import java.time.Duration
+import java.io.InputStreamReader
+
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "1.9.21"
+    id("application")
+    id("org.graalvm.buildtools.native") version "0.9.28"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+}
+
+group = "org.sokangaming"
+version = "1.0-SNAPSHOT"
+
+application {
+    mainClass.set("Main")
+    if (project.hasProperty("mainclass")) {
+        mainClass.set(project.property("mainclass") as String)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = "Main"
+    }
+}
+
+dependencies {
+    implementation("com.google.code.gson:gson:2.10.1")
+    implementation("org.apache.commons:commons-text:1.10.0")
+    implementation("ch.qos.logback:logback-classic:1.4.11")
+    implementation("com.formdev:flatlaf:3.2.5")
+    implementation("dev.dirs:directories:26")
+    implementation("org.apache.xmlgraphics:batik-transcoder:1.17")
+}
+
+
+graalvmNative {
+    agent {
+        defaultMode.set("standard")
+    }
+    metadataRepository {
+        enabled = true
+    }
+    binaries {
+        all {
+            buildArgs.addAll(
+                listOf(
+                    "--no-fallback",
+                    "--enable-https",
+                    "-Djava.awt.headless=false"
+                )
+            )
+            resources.autodetect()
+        }
+    }
+}
+
+fun executeCommandAndHandleOutput(command: Array<String>): Int {
+    val process = Runtime.getRuntime().exec(command)
+
+    // Function to handle stream output
+    fun handleStream(inputStream: InputStream, output: PrintStream) {
+        BufferedReader(InputStreamReader(inputStream)).use { reader ->
+            var line: String?
+            while (reader.readLine().also { line = it } != null) {
+                output.println(line)
+            }
+        }
+    }
+
+    // Creating and starting threads for handling stdout and stderr
+    val stdoutThread = Thread { handleStream(process.inputStream, System.out) }
+    val stderrThread = Thread { handleStream(process.errorStream, System.err) }
+
+    stdoutThread.start()
+    stderrThread.start()
+
+    // Waiting for threads to finish
+    stdoutThread.join()
+    stderrThread.join()
+
+    // Waiting for the process to complete and returning the exit value
+    return process.waitFor()
+}
+
+val operatingSystem = when {
+    System.getProperty("os.name").startsWith("Mac", ignoreCase = true) -> "macosx"
+    System.getProperty("os.name").startsWith("Windows", ignoreCase = true) -> "windows"
+    else -> "linux"
+}
+
+tasks.register<Copy>("setupNativeImage") {
+    // Define the destination directory
+    val destDir = layout.projectDirectory.dir("src/main/resources/META-INF/native-image")
+
+    // Set the destination directory for the copy task
+    into(destDir)
+
+    // Define the source directory
+    val srcDir = layout.projectDirectory.dir("native-image-metadata/$operatingSystem")
+    from(srcDir)
+    include("**/*") // Include all files and directories recursively
+}
+
+tasks.named("processResources") {
+    dependsOn("setupNativeImage")
+}
+
+tasks.named("nativeCompile") {
+    dependsOn("setupNativeImage")
+}
+
+tasks.register("instrument") {
+    doLast {
+        var retval = executeCommandAndHandleOutput(arrayOf("./gradlew", "-Pagent", "run"))
+        if (retval == 0) {
+            retval = executeCommandAndHandleOutput(arrayOf("./gradlew", "metadataCopy", "--task", "run", "--dir", "src/main/resources/META-INF/native-image"))
+            if (retval == 0) {
+                retval = executeCommandAndHandleOutput(arrayOf("./gradlew", "nativeCompile"))
+            }
+        }
+    }
+}
